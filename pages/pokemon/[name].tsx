1842573cf68b0ee9f29693ae754c7d772b223846
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2022-01-08 18:26:48 +0700

    feat: pokemon detail page

--- pages/pokemon/[name].tsx
+++ pages/pokemon/[name].tsx
@@ -1,3 +1,22 @@
+import { GetServerSideProps } from "next";
 import PokemonDetailPage from "../../src/pokemon/containers/PokemonDetailPage";
+import client from "../../src/shared/apollo/client";
+import { GET_POKEMON } from "../../src/shared/apollo/queries";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const {name} = ctx.query
+  const {data} = await client.query({
+    query: GET_POKEMON,
+    variables: {
+      name
+    },
+  })
+  const {pokemon} = data
+  return {
+    props: {
+      pokemon
+    }
+  }
+}
 
 export default PokemonDetailPage
--- src/pokemon/containers/PokemonDetailPage/index.tsx
+++ src/pokemon/containers/PokemonDetailPage/index.tsx
@@ -2,22 +2,23 @@ import { useRouter } from 'next/router'
 import Image from 'next/image'
 import React from 'react'
 
-const PokemonDetailPage = () => {
-  const router = useRouter()
-  const {name} = router.query;
+const PokemonDetailPage = ({pokemon}) => {
+  console.log(pokemon)
   return(
     <div>
-      <h1>{name}</h1>
-      <Image src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png" width="100px" height="100px" />
+      <h1>{pokemon.name}</h1>
+      <Image src={pokemon.sprites.front_default} width="100px" height="100px" />
       <div>
         <div>Pokemon Moves</div>
-        <div>Move 1 Movce 2</div>
-        <div>Move 1 Movce 2</div>
+        {pokemon.moves.map(move => (
+          <div>{move.move.name}</div>
+        ))}
       </div>
       <div>
         <div>Pokemon Types</div>
-        <div>type 1</div>
-        <div>type 2</div>
+        {pokemon.types.map(type => (
+          <div>{type.type.name}</div>
+        ))}
       </div>
     </div>
   )
