44a0259297bd5707e5e38c67d4ed92478824e88d
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2022-01-09 12:17:39 +0700

    deps: add chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
-import { ApolloProvider } from '@apollo/client'
-import Layout from '../src/shared/components/Layout'
-import client from '../src/shared/apollo/client'
-import {PokemonProvider} from '../src/pokemon/provider'
+import { ApolloProvider } from "@apollo/client";
+import Layout from "../src/shared/components/Layout";
+import client from "../src/shared/apollo/client";
+import { PokemonProvider } from "../src/pokemon/provider";
+import { ChakraProvider } from "@chakra-ui/react";
 const App = ({ Component, pageProps }) => (
-      <ApolloProvider client={client} >
-        <PokemonProvider>
+  <ApolloProvider client={client}>
+    <ChakraProvider>
+      <PokemonProvider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
-        </PokemonProvider>
-      </ApolloProvider>
-)
+      </PokemonProvider>
+    </ChakraProvider>
+  </ApolloProvider>
+);
 
-export default App
+export default App;
--- src/pokemon/components/PokemonCard/index.tsx
+++ src/pokemon/components/PokemonCard/index.tsx
@@ -1,38 +1,46 @@
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import React, { useContext } from 'react'
-import { PokemonResponse } from '../../containers/AllPokemonPage'
-import { PokemonContext } from '../../context'
-import { PokemonCardWrapper } from './styles'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useContext } from "react";
+import { PokemonResponse } from "../../containers/AllPokemonPage";
+import { PokemonContext } from "../../context";
+import { PokemonCardWrapper } from "./styles";
 
 type PokemonCardProps = {
-  pokemon: PokemonResponse,
-  nickname?: string
-}
+  pokemon: PokemonResponse;
+  nickname?: string;
+};
 
-const PokemonCard = ({pokemon, nickname}:PokemonCardProps) => {
-  const {releasePokemon} = useContext(PokemonContext)
-  const router = useRouter()
-  const handleClickPokemonCard = (name:string) => {
-    router.push(`/pokemon/${name}`)
-  }
+const PokemonCard = ({ pokemon, nickname }: PokemonCardProps) => {
+  const { releasePokemon } = useContext(PokemonContext);
+  const router = useRouter();
+  const handleClickPokemonCard = (name: string) => {
+    router.push(`/pokemon/${name}`);
+  };
 
   const handleReleasePokemon = () => {
-    releasePokemon(nickname, pokemon)
-  }
+    releasePokemon(nickname, pokemon);
+  };
 
-  return(
-    <PokemonCardWrapper onClick={() => !nickname && handleClickPokemonCard(pokemon.name)} >
-      <div className='pokemon-id-and-owned-row' >
+  return (
+    <PokemonCardWrapper>
+      <div className="pokemon-id-and-owned-row">
         <div>{pokemon.id}</div>
         <div>00 owned</div>
-       </div>
-      <Image src={pokemon.image} objectFit="contain" width={250} height={250} />
+      </div>
+      <Image
+        onClick={() => handleClickPokemonCard(pokemon.name)}
+        src={pokemon.image}
+        objectFit="contain"
+        width={250}
+        height={250}
+      />
       <div>{nickname ? nickname : ""}</div>
       <div>{pokemon.name}</div>
-      {nickname && <button onClick={() => handleReleasePokemon()} >Release</button>}
+      {nickname && (
+        <button onClick={() => handleReleasePokemon()}>Release</button>
+      )}
     </PokemonCardWrapper>
-  )
-}
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
